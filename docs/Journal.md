030d89bcbf29b0eb99a17c11749627a7ed2c3d8c
Willow Cunningham <willow.e.cunningham@gmail.com>
2025-03-29 21:01:13 -0400

    added test script that can time training a linear model with a ton of parameters

--- docs/Journal.md
+++ docs/Journal.md
@@ -6,3 +6,7 @@ This document contains notes, thoughts, todos, etc considered in the development
 ## 3/29/2025
 
 Installing pytorch to get started. On the broadwell machine, need to use apt. made the mistake of `sudo apt update && sudo apt upgrade`-ing, which apparently hasnt been done in a while. To install, will `sudo apt-get install python3-torch`.
+
+Went ahead and installed on raptorlake while waiting. Now to download the examples from [pytorch/examples](https://github.com/pytorch/examples/tree/main)
+
+Okay, so what I want to do actually is characterize individual *layers* and then we can just know what we are dealing with. That easy right?
---
+++ src/test.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torch.utils.data import TensorDataset, DataLoader
+
+import argparse
+import time
+
+class Net(nn.Module):
+    def __init__(self, input_size: int, hidden_sizes: list, num_classes: int):
+        super(Net,self).__init__()
+        
+        self.layers = nn.ModuleList([nn.Linear(input_size, hidden_sizes[0])])
+        if len(hidden_sizes) > 1:
+            for i in range(1, len(hidden_sizes)-1):
+                self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
+                self.layers.append(nn.ReLU())
+
+        self.layers.append(nn.Linear(hidden_sizes[-1], num_classes))
+
+
+    def forward(self,x):
+    
+        out = self.layers[0](x)
+        if len(self.layers) > 0:
+            for layer in self.layers[1:-1]:
+                out = layer(out)
+
+        out = self.layers[-1](out)
+
+        return out
+
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+
+        loss = F.mse_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ds", "--data_size", type=int, default=100, help="size of the randomzied dataset")
+    parser.add_argument("-hs", "--hidden_size", type=int, default=128, help="size of the hidden layer")
+    parser.add_argument("-nl", "--n_layers", type=int, default=4, help="number of hidden layers")
+    parser.add_argument("-bs", "--batch_size", type=int, default=64, help="batch_size")
+    parser.add_argument("-e", "--epochs", type=int, default=5, help="number of epochs")
+    parser.add_argument("-is", "--input_size", type=int, default=32, help="input size")
+
+    args = parser.parse_args()
+
+    device = "cpu"
+
+    model = Net(args.input_size, [args.hidden_size for i in range(args.n_layers)], 1)
+    for layer in model.children():
+        print(layer)
+
+    training_X = torch.rand((args.data_size, args.input_size))
+    training_y = torch.rand((args.data_size, 1))
+    train_dataloader = DataLoader(TensorDataset(training_X, training_y), batch_size=args.batch_size, shuffle=True)
+
+    optimizer = optim.Adadelta(model.parameters(), lr=1e-3)
+
+    print("Training")
+    start = time.time()
+    for epoch in range(args.epochs):
+        print(".", end='', flush=True)
+        train(model, device, train_dataloader, optimizer, epoch)
+
+    print(f"\nDone!. Took {time.time()-start:.4f}s.")
+
