b49da03713b6479d8d73a602407a517155347c6d
Willow Cunningham <willow.e.cunningham@gmail.com>
2025-03-30 11:01:35 -0400

    got data for nl 0-100

--- src/bench.py
+++ src/bench.py
@@ -2,21 +2,41 @@ import json
 from subprocess import Popen
 import os, argparse
 
-def script_energy(script: str, args: list=[]):
+
+def args_dict_to_list(args_dict: dict):
+    '''
+    converts a dictionary of command line args e.g.
+        {'-e': 3, '-nl': 10, '-v': ''}
+    to a list passable to Popen like
+        ['-e', '3', '-nl', '10', '-v', '']
+    '''
+    args_list = []
+    for k, v in args_dict.items():
+        args_list.append(str(k))
+        args_list.append(str(v))
+    return args_list
+
+
+def script_energy(script: str, n_avg=10, args_dict: dict={}):
     '''
     runs a python script with perf stat energy-cores and args 
     '''
-    # bench the energy costs of the test target
-    p1 = Popen(['perf', 'stat', '--event', 'energy-cores', '-j', '-o', 'tmp.json', 'python3'] + [script] + args)
-    err = p1.wait()
 
-    # data is now in tmp.json
-    with open('tmp.json', 'r') as f:
-        next(f) # discard the comment header perf adds
-        data = json.load(f)
-    os.remove('tmp.json')
+    args_list = args_dict_to_list(args_dict)
+    energies = []
+    for _ in range(n_avg):
+        # bench the energy costs of the test target
+        p1 = Popen(['perf', 'stat', '--event', 'energy-cores', '-j', '-o', 'tmp.json', 'python3'] + [script] + args_list)
+        err = p1.wait()
 
-    return data['counter-value']
+        # data is now in tmp.json
+        with open('tmp.json', 'r') as f:
+            next(f) # discard the comment header perf adds
+            data = json.load(f)
+        os.remove('tmp.json')
+        energies.append(float(data['counter-value']))
+
+    return sum(energies) / n_avg
 
 
 if __name__ == "__main__":
@@ -26,9 +46,13 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-    energies = []
-    for i in range(args.n_avg):
-        energies.append(float(script_energy('linear.py', args=['-e', '3'])))
-        print(f'{i}: {energies[i]:.2f}')
 
-    print(f'average: {sum(energies)/args.n_avg:.2f}J')
+    energy = []
+    num_layers = []
+    for i in range(1, 100):
+        linear_args = {'-e': 1, '-nl': i}
+        energy.append(script_energy('linear.py', args.n_avg, linear_args))
+
+        print(linear_args, energy[i-1])
+
+    print(energy)
