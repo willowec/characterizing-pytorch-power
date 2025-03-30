ad301e0de4c56d5250152856d234599e068f014b
Willow Cunningham <willow.e.cunningham@gmail.com>
2025-03-29 21:23:45 -0400

    made parameterized function for test

--- docs/Journal.md
+++ docs/Journal.md
@@ -10,3 +10,6 @@ Installing pytorch to get started. On the broadwell machine, need to use apt. ma
 Went ahead and installed on raptorlake while waiting. Now to download the examples from [pytorch/examples](https://github.com/pytorch/examples/tree/main)
 
 Okay, so what I want to do actually is characterize individual *layers* and then we can just know what we are dealing with. That easy right?
+
+As a first start, I created a script that can define a net that just has linear layers with a ton of parameters, and times it.
+Next, I should make it into an importable function and set up an energy test fixture script
--- src/test.py
+++ src/test.py
@@ -13,6 +13,7 @@ class Net(nn.Module):
         super(Net,self).__init__()
         
         self.layers = nn.ModuleList([nn.Linear(input_size, hidden_sizes[0])])
+        self.layers.append(nn.ReLU())
         if len(hidden_sizes) > 1:
             for i in range(1, len(hidden_sizes)-1):
                 self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
@@ -45,6 +46,33 @@ def train(model, device, train_loader, optimizer, epoch):
         optimizer.step()
 
 
+def simulate_training(ds, nl, hs, e, bs, print_layers=False):
+    
+    device = "cpu"
+    input_size=32
+
+    model = Net(input_size, [hs for i in range(nl)], 1)
+    
+    if print_layers:
+        for layer in model.children():
+            print(layer)
+
+    training_X = torch.rand((ds, input_size))
+    training_y = torch.rand((ds, 1))
+    train_dataloader = DataLoader(TensorDataset(training_X, training_y), batch_size=bs, shuffle=True)
+
+    optimizer = optim.Adadelta(model.parameters(), lr=1e-3)
+
+    print("Training")
+    start = time.time()
+    for epoch in range(e):
+        print(".", end='', flush=True)
+        train(model, device, train_dataloader, optimizer, epoch)
+
+    print(f"\nDone!. Took {time.time()-start:.4f}s.")
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-ds", "--data_size", type=int, default=100, help="size of the randomzied dataset")
@@ -52,27 +80,8 @@ if __name__ == "__main__":
     parser.add_argument("-nl", "--n_layers", type=int, default=4, help="number of hidden layers")
     parser.add_argument("-bs", "--batch_size", type=int, default=64, help="batch_size")
     parser.add_argument("-e", "--epochs", type=int, default=5, help="number of epochs")
-    parser.add_argument("-is", "--input_size", type=int, default=32, help="input size")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
 
     args = parser.parse_args()
 
-    device = "cpu"
-
-    model = Net(args.input_size, [args.hidden_size for i in range(args.n_layers)], 1)
-    for layer in model.children():
-        print(layer)
-
-    training_X = torch.rand((args.data_size, args.input_size))
-    training_y = torch.rand((args.data_size, 1))
-    train_dataloader = DataLoader(TensorDataset(training_X, training_y), batch_size=args.batch_size, shuffle=True)
-
-    optimizer = optim.Adadelta(model.parameters(), lr=1e-3)
-
-    print("Training")
-    start = time.time()
-    for epoch in range(args.epochs):
-        print(".", end='', flush=True)
-        train(model, device, train_dataloader, optimizer, epoch)
-
-    print(f"\nDone!. Took {time.time()-start:.4f}s.")
-
+    simulate_training(args.data_size, args.n_layers, args.hidden_size, args.epochs, args.batch_size, print_layers=args.verbose)
