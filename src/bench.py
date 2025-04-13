d18db36c0f934deca4aaa39178668dd62cc02329
Willow Cunningham <willow.e.cunningham@gmail.com>
2025-04-13 17:30:41 -0400

    setting up for CNN, Linear parameter space exploration

--- src/bench.py
+++ src/bench.py
@@ -1,6 +1,8 @@
 import json
 from subprocess import Popen
 import os, argparse
+import sqlite3
+import time
 
 
 def args_dict_to_list(args_dict: dict):
@@ -17,42 +19,105 @@ def args_dict_to_list(args_dict: dict):
     return args_list
 
 
-def script_energy(script: str, n_avg=10, args_dict: dict={}):
+def script_energy(script: str, args_dict: dict={}):
     '''
-    runs a python script with perf stat energy-cores and args 
+    runs a python script with perf stat energy-cores and args and returns energy, runtime
     '''
 
     args_list = args_dict_to_list(args_dict)
-    energies = []
-    for _ in range(n_avg):
-        # bench the energy costs of the test target
-        p1 = Popen(['perf', 'stat', '--event', 'energy-cores', '-j', '-o', 'tmp.json', 'python3'] + [script] + args_list)
-        err = p1.wait()
+    
+    # bench the energy costs of the test target
+    tstart = time.time()
+    p1 = Popen(['perf', 'stat', '--event', 'energy-cores', '-j', '-o', 'tmp.json', 'python3'] + [script] + args_list)
+    err = p1.wait()
+    tend = time.time()
 
-        # data is now in tmp.json
-        with open('tmp.json', 'r') as f:
-            next(f) # discard the comment header perf adds
-            data = json.load(f)
-        os.remove('tmp.json')
-        energies.append(float(data['counter-value']))
+    # data is now in tmp.json
+    with open('tmp.json', 'r') as f:
+        next(f) # discard the comment header perf adds
+        data = json.load(f)
+    os.remove('tmp.json')
 
-    return sum(energies) / n_avg
+    return float(data['counter-value']), tend - tstart
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('-n', '--n_avg', type=int, default=10, help='number of tests to average over')
     
     args = parser.parse_args()
 
+    start_time = time.time()
 
-    energy = []
-    num_layers = []
-    for i in range(1, 100):
-        linear_args = {'-e': 1, '-nl': i}
-        energy.append(script_energy('linear.py', args.n_avg, linear_args))
+    # open database
+    con = sqlite3.connect(f'{os.uname().nodename}_energy.db')
+    cur = con.cursor()
 
-        print(linear_args, energy[i-1])
+    # create linear table
+    cur.execute('CREATE TABLE IF NOT EXISTS linear(data_size INTEGER, epochs INTEGER, batch_size INTEGER, n_layers INTEGER, hidden_size INTEGER, energy REAL, traintime REAL)')
 
-    print(energy)
+    # define iterations for each thingy. start, stop, step
+    L_DATA_SIZE = [128, 1024, 128]
+    L_EPOCHS = [10, 100, 10]
+    L_BATCH_SIZE = [32, 512, 32]
+    L_N_LAYERS = [1, 50, 1]
+    L_HIDDEN_SIZE = [16, 256, 16]
+
+    for data_size in range(*L_DATA_SIZE):
+        for epochs in range(*L_EPOCHS):
+            for batch_size in range (*L_BATCH_SIZE):
+                for n_layers in range (*L_N_LAYERS):
+                    for hidden_size in range(*L_HIDDEN_SIZE):
+                        linear_args = {'-e': epochs, '-nl': n_layers, 
+                                       '-ds': data_size, '-hs': hidden_size,
+                                       '-bs': batch_size}
+
+                        energy, traintime = script_energy('linear.py', linear_args)
+
+                        print(f'linear {time.time()-start_time:.3e}s:\t{data_size=} {epochs=} {batch_size=} {n_layers=} {hidden_size=}: {energy=:.2f}J {traintime=:.2f}s')
+
+                        cur.execute('INSERT INTO linear VALUES(?, ?, ?, ?, ?, ?, ?)',
+                                    [data_size, epochs, batch_size, n_layers,
+                                    hidden_size, energy, traintime])
+                        con.commit()
+
+
+    # create cnn table
+    cur.execute('CREATE TABLE IF NOT EXISTS cnn(data_size INTEGER, epochs INTEGER, batch_size INTEGER, n_layers INTEGER, channels INTEGER, k_size INTEGER, stride INTEGER, side_length INTEGER, energy REAL, traintime REAL)')
+
+    # define iterations for each thingy. start, stop, step
+    C_DATA_SIZE = [128, 1024, 128]
+    C_EPOCHS = [10, 100, 10]
+    C_BATCH_SIZE = [32, 512, 32]
+    C_N_LAYERS = [1, 50, 1]
+    C_CHANNELS = [16, 128, 16]
+    C_K_SIZE = [3, 9, 1]
+    C_STRIDE = [1, 4, 1]
+    C_SIDE_LENGTH = [16, 128, 16]
+
+    for data_size in range(*C_DATA_SIZE):
+        for epochs in range(*C_EPOCHS):
+            for batch_size in range (*C_BATCH_SIZE):
+                for n_layers in range (*C_N_LAYERS):
+                    for channels in range(*C_CHANNELS):
+                        for k_size in range (*C_K_SIZE):
+                            for stride in range(*C_STRIDE):
+                                for side_length in range(*C_SIDE_LENGTH):
+                                    cnn_args = {'-e': epochs, 
+                                                   '-nl': n_layers, 
+                                                   '-ds': data_size, 
+                                                   '-cs': channels,
+                                                   '-bs': batch_size,
+                                                   '-st': stride,
+                                                   '-sl': side_length,
+                                                   '-ks': k_size,
+                                                   }
+                                    
+                                    energy, traintime = script_energy('cnn.py', cnn_args)
+
+                                    print(f'cnn {time.time()-start_time:.3e}s:\t{data_size=} {epochs=} {batch_size=} {n_layers=} {channels=} {k_size=} {stride=} {side_length=}: {energy=:.2f}J {traintime=:.2f}s')
+
+                                    cur.execute('INSERT INTO cnn VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                                [data_size, epochs, batch_size, n_layers,
+                                                channels, k_size, stride, side_length, 
+                                                energy, traintime])
+                                    con.commit()
--- src/cnn.py
+++ src/cnn.py
@@ -9,17 +9,15 @@ import argparse
 import time
 
 class Net(nn.Module):
-    def __init__(self, input_size: int, hidden_sizes: list, num_classes: int):
+    def __init__(self, layers, channels, k_size, stride):
         super(Net,self).__init__()
         
-        self.layers = nn.ModuleList([nn.Linear(input_size, hidden_sizes[0])])
-        self.layers.append(nn.ReLU())
-        if len(hidden_sizes) > 1:
-            for i in range(1, len(hidden_sizes)-1):
-                self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
-                self.layers.append(nn.ReLU())
+        self.layers = nn.ModuleList([nn.Conv2d(channels, channels, k_size, stride=stride, padding='same')])
+        if layers > 1:
+            for i in range(1, layers-1):
+                self.layers.append(nn.Conv2d(channels, channels, k_size, stride=stride, padding='same'))
 
-        self.layers.append(nn.Linear(hidden_sizes[-1], num_classes))
+        self.layers.append(nn.Conv2d(channels, channels, k_size, stride=stride, padding='same'))
 
 
     def forward(self,x):
@@ -31,32 +29,36 @@ class Net(nn.Module):
 
         out = self.layers[-1](out)
 
+        # get to size 1 for label
+        out = torch.mean(out, (1, 2, 3))
+
         return out
 
 
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(device), torch.squeeze(target.to(device))
         optimizer.zero_grad()
-        output = model(data)
+        output = torch.squeeze(model(data))
 
         loss = F.mse_loss(output, target)
         loss.backward()
         optimizer.step()
 
 
-def simulate_training(ds, nl, hs, e, bs, verbose=False):
+def simulate_training(epochs, ds, x_size, y_size, n_layers, channels, k_size, stride, bs, verbose=False):
     
     device = "cpu"
 
-    model = Net(hs, [hs for i in range(nl)], 1)
+    model = Net(channels, channels, k_size, stride)
     
     if verbose:
         for layer in model.children():
             print(layer)
 
-    training_X = torch.rand((ds, hs))
+    imsize = (channels, x_size, y_size)
+    training_X = torch.rand((ds, *imsize))
     training_y = torch.rand((ds, 1))
     train_dataloader = DataLoader(TensorDataset(training_X, training_y), batch_size=bs, shuffle=True)
 
@@ -66,7 +68,7 @@ def simulate_training(ds, nl, hs, e, bs, verbose=False):
         print("Training")
     
     start = time.time()
-    for epoch in range(e):
+    for epoch in range(epochs):
         if verbose:
             print(".", end='', flush=True)
         train(model, device, train_dataloader, optimizer, epoch)
@@ -78,12 +80,18 @@ def simulate_training(ds, nl, hs, e, bs, verbose=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-ds", "--data_size", type=int, default=100, help="size of the randomzied dataset")
-    parser.add_argument("-hs", "--hidden_size", type=int, default=128, help="size of the hidden layer")
     parser.add_argument("-nl", "--n_layers", type=int, default=4, help="number of hidden layers")
     parser.add_argument("-bs", "--batch_size", type=int, default=64, help="batch_size")
     parser.add_argument("-e", "--epochs", type=int, default=5, help="number of epochs")
+    parser.add_argument("-cs", "--channels", type=int, default=3, help="number of channels")
+    parser.add_argument("-ks", "--k_size", type=int, default=3, help="kernel size")
+    parser.add_argument("-st", "--stride", type=int, default=1, help="stride")
+    parser.add_argument("-sl", "--side_length", type=int, default=32, help="image side length")
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
 
     args = parser.parse_args()
 
-    simulate_training(args.data_size, args.n_layers, args.hidden_size, args.epochs, args.batch_size, verbose=args.verbose)
+    simulate_training(args.epochs, args.data_size, 
+                      args.side_length, args.side_length, args.n_layers, 
+                      args.channels, args.k_size, args.stride, 
+                      args.batch_size, verbose=args.verbose)
